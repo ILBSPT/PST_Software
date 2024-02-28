f8a101ee870aa1419de6258c17080660a357b3de
ILBSPT <andremanada@gmail.com>
2024-02-28 08:20:47 +0000

    logger start

--- FlightComputer/src/StMWork.cpp
+++ FlightComputer/src/StMWork.cpp
@@ -5,6 +5,7 @@
 #include "HardwareCfg.h"
 #include "StMWork.h"
 #include "GlobalVars.h"
+#include "Comms.h"
 
 #include <I2Cdev.h>
 #include <MPU6050.h>
@@ -119,7 +120,26 @@ void read_IMU(void)
 
 void logger(void)
 {
+    command_t command_rep;
+    command_rep.cmd = CMD_STATUS;
+    command_rep.size = 2*6 + 1;
+    //command_rep.size = 100; //test
+    command_rep.data[0] = state;
+    command_rep.data[1] = (imu_ax >> 8) & 0xff;
+    command_rep.data[2] = (imu_ax) & 0xff ;
+    command_rep.data[3] = (imu_ay >> 8) & 0xff;
+    command_rep.data[4] = (imu_ay) & 0xff;
+    command_rep.data[5] = (imu_az >> 8) & 0xff;
+    command_rep.data[6] = (imu_az) & 0xff;
+    command_rep.data[7] = (imu_gx >> 8) & 0xff;
+    command_rep.data[8] = (imu_gx) & 0xff;
+    command_rep.data[9] = (imu_gy >> 8) & 0xff;
+    command_rep.data[10] = (imu_gy) & 0xff;
+    command_rep.data[11] = (imu_gz >> 8) & 0xff;
+    command_rep.data[12] = (imu_gz) & 0xff;
+    command_rep.crc = 0x5151;
 
+    write_command(&command_rep);
 }
 
 void pressure_safety(void)
---
+++ HelperCode/data_portal.py
@@ -0,0 +1,204 @@
+import PySimpleGUI as sg
+import time
+import serial
+
+#mac
+#ser = serial.Serial('/dev/cu.usbserial-0001', 115200, timeout=0.015) #set read timeout of 1s
+#windows
+#ser = serial.Serial('COM3', 115200, timeout=0.01) #set read timeout of 1s
+
+log_delay = 0.01
+message_timeout = 0.3
+
+missed_packets = 0
+log_speed = 0
+last_log = 0
+
+command_map = {
+    "STATUS" : 0,
+    "READY" : 1,
+    "ARM" : 2,
+    "ABORT" : 3,
+    "LED_ON" : 4,
+    "LED_OFF" : 5,
+    "IMU_CALIB" : 6, 
+    "STATUS_ACK" : 8,
+    "ARM_ACK" : 10
+}
+
+state_map = {
+    "IDLE" : 0,
+    "READY" : 1,
+    "ARMED" : 2,
+    "LAUNCH": 3,
+    "ABORT": 4,
+    "IMU_CALIB": 5
+}
+
+state_map_to_string = {
+    0: "IDLE",
+    1: "READY",
+    2: "ARMED",
+    3: "LAUNCH",
+    4: "ABORT",
+    5: "IMU_CALIB"
+}
+
+sync_state = 1
+cmd_state = 2
+size_state = 3
+data_state = 4
+crc_1_state = 5
+crc_2_state = 6
+end_state = 7
+
+comm_state = sync_state
+data_total = 0
+data_recv = 0
+begin = time.perf_counter()
+end = time.perf_counter()
+buff = bytearray()
+
+sg.theme('DarkAmber')    # Keep things interesting for your users
+
+layout = [[sg.Text("Ax  Ay  Az  Gx  Gy  Gz", key = '_IMU_OUT_', size = (40, 5), auto_size_text=True, font=('Arial Bold', 16))],      
+          [sg.Exit()]]      
+
+window = sg.Window('Window that stays open', layout)      
+
+def print_status():
+    global missed_packets
+    global log_speed
+    if(len(buff) < 16):
+        print("bad status")
+        return
+
+    state = int.from_bytes(buff[3:4], byteorder='big', signed=True)
+    if state < 0 or state >= len(state_map_to_string): 
+        print("bad state decoding")
+        return
+
+    ax = int.from_bytes(buff[4:6], byteorder='big', signed=True)
+    ay = int.from_bytes(buff[6:8], byteorder='big', signed=True)
+    az = int.from_bytes(buff[8:10], byteorder='big', signed=True)
+
+    ax = (ax * 0.244) / 1000 * 9.80665
+    ay = (ay * 0.244) / 1000 * 9.80665
+    az = (az * 0.244) / 1000 * 9.80665
+
+    gx = int.from_bytes(buff[10:12], byteorder='big', signed=True)
+    gy = int.from_bytes(buff[12:14], byteorder='big', signed=True)
+    gz = int.from_bytes(buff[14:16], byteorder='big', signed=True)
+
+    print("state", state)
+    s1 = "State: " + state_map_to_string[state] + "\n"
+    sa = "Ax: " + str(round(ax, 2)) + " Ay: " + str(round(ay, 2)) + " Az: " + str(round(az, 2)) + "\n"
+    sg = "Gx: " + str(round(gx, 2)) + " Gy: " + str(round(gy, 2)) + " Gz: " + str(round(gz, 2)) + "\n"
+    s2 = "Log Speed: " + str(round(log_speed, 0)) + "hz\nMissed packets: " + str(missed_packets) + "\n" 
+    s = s1 + sa + sg + s2
+    window['_IMU_OUT_'].update(s)
+
+    print("ax:", ax, "ay:", ay, "az:", az)
+    print("gx:", gx, "gy:", gy, "gz:", gz)
+
+def read_cmd():
+    global comm_state, sync_state, cmd_state, size_state, data_state, crc_1_state, crc_2_state, end_state
+    global buff
+    global data_recv, data_total
+    global begin, end
+    global missed_packets
+    global last_log, log_speed
+
+    buff = bytearray()
+    data_recv = 0
+    begin = time.perf_counter()
+    
+    while ser.in_waiting > 0:
+        ch = ser.read(1)
+        ch = int.from_bytes(ch, 'little')
+
+        if comm_state == sync_state:
+            if ch == 0x55:
+                buff = bytearray()
+                data_recv = 0
+                begin = time.perf_counter()
+                comm_state = cmd_state
+                buff.append(ch)
+            else:
+                print(chr(ch), end="")
+                #print(str(ch).encode('utf-8'))
+
+        elif comm_state == cmd_state:
+            buff.append(ch)
+            comm_state = size_state
+
+        elif comm_state == size_state:
+            buff.append(ch)
+            data_total = int(ch)
+            if data_total == 0:
+                comm_state = crc_1_state
+            else:
+                comm_state = data_state
+
+        elif comm_state == data_state:
+            buff.append(ch)
+            data_recv += 1
+            if data_total == data_recv:
+                comm_state = crc_1_state
+
+        elif comm_state == crc_1_state:
+            buff.append(ch)
+            comm_state = crc_2_state
+
+        elif comm_state == crc_2_state: 
+            buff.append(ch)
+            comm_state = end_state
+    
+    end = time.perf_counter()
+    msec = (end - begin) 
+    #if(comm_state != sync_state): print("ms: ", msec)
+    if comm_state != sync_state and msec > message_timeout:
+        comm_state = sync_state
+        missed_packets += 1
+        print("command timeout", msec)
+        print("buffer recieved:", buff) 
+        return
+
+    if msec > message_timeout:
+        print("command timeout", msec)
+        print("no cmd state: ", comm_state)
+        missed_packets += 1
+        return
+
+    if comm_state == end_state:
+        comm_state = sync_state
+        print("ACK recieved:", len(buff))
+        print("command time", msec)
+        log_speed = 1 / ( end - last_log )
+        if(buff[1] == command_map['STATUS_ACK']):
+            print_status()
+        last_log = end
+        print("Cmd_ACK: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
+        #print(buff)
+        return
+
+
+
+log_begin = time.perf_counter()
+log_end = time.perf_counter()
+
+while True:           
+    event, values = window.read(timeout=1) 
+    if event != '__TIMEOUT__': print(event, values)       
+    cmd_launched = 0
+
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break      
+    
+    #while ser.in_waiting > 0:
+        #print(ser.read(1).decode('utf-8'), end='')
+
+    #read_cmd()
+    time.sleep(log_delay * 0.1)
+
+window.close()
