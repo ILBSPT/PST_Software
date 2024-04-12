37b4e1586d5e49eb79b783f96cca7d80cbb8116c
ILBSPT <andremanada@gmail.com>
2024-04-12 16:48:29 +0100

    interface change for each computer

--- HelperCode/gui_test_interface.py
+++ HelperCode/gui_test_interface.py
@@ -1,16 +1,21 @@
 import PySimpleGUI as sg
 import time
 import serial
+import sys
 
 #mac
-ser = serial.Serial('/dev/cu.usbserial-0001', 115200, timeout=0.015) #set read timeout of 1s
+#ser = serial.Serial('/dev/cu.usbserial-0001', 115200, timeout=0.015) #set read timeout of 1s
 #ser = serial.Serial('/dev/cu.usbserial-3', 115200, timeout=0.015) #set read timeout of 1s
 #windows
 #ser = serial.Serial('COM3', 115200, timeout=0.01) #set read timeout of 1s
+if(len(sys.argv) > 2):
+    ser = serial.Serial(sys.argv[2], 115200, timeout=0.001) #set read timeout of 1s
+else:
+    ser = serial.Serial('COM3', 115200, timeout=0.001) #set read timeout of 1s
 
 log_delay = 0.01
 #log_delay = 1
-message_timeout = 0.5
+message_timeout = 0.25
 
 missed_packets = 0
 log_speed = 0
@@ -34,27 +39,21 @@ command_map = {
 #FILLING station commands
     "RESUME_PROG" : 10,
 
-#State machine commands
-    "ADD_WORK" : 11,
-    "REMOVE_WORK" : 12,
-
 #used to get the number of commands 
-    "cmd_size" : 13,
+    "cmd_size" : 11,
 
 #ACKs
-    "STATUS_ACK" : 14,
-    "ABORT_ACK" : 15, 
-    "EXEC_PROG_ACK" : 16,
-    "STOP_PROG_ACK" : 17,
-    "FUELING_ACK" : 18,
-    "READY_ACK" : 19,
-    "ARM_ACK" : 20,
-    "LED_ON_ACK" : 21,
-    "LED_OFF_ACK" : 22,
-    "IMU_CALIB_ACK" : 23,
-    "RESUME_PROG_ACK" : 24,
-    "ADD_WORK_ACK" : 25,
-    "REMOVE_WORK_ACK" : 26 
+    "STATUS_ACK" : 12,
+    "ABORT_ACK" : 13, 
+    "EXEC_PROG_ACK" : 14,
+    "STOP_PROG_ACK" : 15,
+    "FUELING_ACK" : 16,
+    "READY_ACK" : 17,
+    "ARM_ACK" : 18,
+    "LED_ON_ACK" : 19,
+    "LED_OFF_ACK" : 20,
+    "IMU_CALIB_ACK" : 21,
+    "RESUME_PROG_ACK" : 22,
 }
 
 state_map_fill = {
@@ -103,8 +102,6 @@ state_map_to_string_rocket = {
     9 : "IMU_CALIB"
 }
 
-state_map = state_map_fill
-state_map_to_string = state_map_to_string_fill
 
 
 sync_state = 1
@@ -128,7 +125,8 @@ layout_rocket = [[sg.Button('LED_ON', key = '_LED_ON_', size = (10,5)), sg.Butto
           [sg.Button('Start Fueling', key = '_FUELING_', size = (10, 5)),
            sg.Button('Stop', key = '_STOP_', size = (10, 5)),
            sg.Button('Exec prog', key = '_EXEC_', size = (10, 5)),
-           sg.Combo(values = (1,2), default_value = 1, key = '_PROG_', size = (10,5), font=('Arial Bold', 16))],
+           #sg.Combo(values = (1,2), default_value = 1, key = '_PROG_', size = (10,5), font=('Arial Bold', 16)),
+           sg.Input(key = '_PROG_', size = (20,5), font=('Arial Bold', 16))],
           [sg.Button('Ready', key = '_READY_', size = (10,5)),
            sg.Button('Arm', key = '_ARM_', size = (10,5)), 
            sg.Button('Abort', key = '_ABORT_', size = (10,5)), 
@@ -138,10 +136,8 @@ layout_rocket = [[sg.Button('LED_ON', key = '_LED_ON_', size = (10,5)), sg.Butto
 
 layout_fill = [[sg.Button('LED_ON', key = '_LED_ON_', size = (10,5)), sg.Button('LED_OFF', key = '_LED_OFF_', size = (10,5)), sg.Text("Ax  Ay  Az  Gx  Gy  Gz", key = '_IMU_OUT_', size = (40, 5), auto_size_text=True, font=('Arial Bold', 16))],      
           [sg.Button('Exec prog', key = '_EXEC_', size = (10, 5)),
-           sg.Combo(values = (1,2,3), default_value = 1, key = '_PROG_', size = (10,5), font=('Arial Bold', 16)),
-           sg.Button('Change', key = '_CHANGE_', size = (10, 5)), 
-           sg.Combo(values = ('FP1','FP2', 'FP3', 'FL1', 'FL2'), default_value = 'FP1', key = '_VAR_', size = (10,5), font=('Arial Bold', 16)),
-           sg.Input(default_text = "", key = '_CHANGE_VALUE_', size = (10, 5), font=('Arial Bold', 16))],
+           #sg.Combo(values = (1,2,3), default_value = 1, key = '_PROG_', size = (10,5), font=('Arial Bold', 16)),
+           sg.Input(key = '_PROG_', size = (20,5), font=('Arial Bold', 16))],
           [sg.Button('Start Fueling', key = '_FUELING_', size = (10, 5)),
            sg.Button('Stop', key = '_STOP_', size = (10, 5)),
            sg.Button('Resume', key = '_RESUME_', size = (10, 5)), sg.Button('Abort', key = '_ABORT_', size = (10,5))],
@@ -149,7 +145,19 @@ layout_fill = [[sg.Button('LED_ON', key = '_LED_ON_', size = (10,5)), sg.Button(
            sg.Button('Toggle log', key = '_LOG_TOGGLE_', size = (10,5)), sg.Exit()]]  
 
 #layout = layout_rocket
-layout = layout_fill
+if(len(sys.argv) > 1):
+    if(sys.argv[1] == 'r'):
+        layout = layout_rocket
+        state_map = state_map_rocket
+        state_map_to_string = state_map_to_string_rocket
+    elif(sys.argv[1] == 'f'):
+        layout = layout_fill
+        state_map = state_map_fill
+        state_map_to_string = state_map_to_string_fill
+else:
+    layout = layout_rocket
+    state_map = state_map_rocket
+    state_map_to_string = state_map_to_string_rocket
 
 window = sg.Window('Window that stays open', layout)      
 
@@ -221,11 +229,12 @@ def print_status():
     #sg = "Gx: " + str(round(gx, 2)) + " Gy: " + str(round(gy, 2)) + " Gz: " + str(round(gz, 2)) + "\n"
 
     sp = "Pressure: " + str(int.from_bytes(buff[4:6], byteorder='big', signed=False)) + "\n"
+    sl = "Tank Liquid: " + str(int.from_bytes(buff[6:8], byteorder='big', signed=False)) + "\n"
 
     s2 = "Log Speed: " + str(round(log_speed, 0)) + "hz\nMissed packets: " + str(missed_packets) + "\n" 
 
     #s = s1 + sa + sg + s2
-    s = s1 + sp + s2
+    s = s1 + sl + sp + s2
 
     window['_IMU_OUT_'].update(s)
 
@@ -264,6 +273,7 @@ def read_cmd():
             elif comm_state == size_state:
                 buff.append(ch)
                 data_total = int(ch)
+                #print("data total", data_total)
                 if data_total == 0:
                     comm_state = crc_1_state
                 else:
@@ -282,32 +292,41 @@ def read_cmd():
             elif comm_state == crc_2_state: 
                 buff.append(ch)
                 comm_state = end_state
+            
+            #print("Cmd rs485 byte recv",ch, comm_state, ser.in_waiting)
         
         end = time.perf_counter()
         msec = (end - begin) 
-        #if(comm_state != sync_state): print("ms: ", msec)
-        if comm_state != sync_state and msec > message_timeout:
-            comm_state = sync_state
-            missed_packets += 1
-            print("command timeout", msec)
-            print("buffer recieved:", buff) 
-            return
-
-        if msec > message_timeout:
-            print("command timeout", msec)
-            print("no cmd state: ", comm_state)
-            missed_packets += 1
-            return
-
+        
         if comm_state == end_state:
             comm_state = sync_state
             print("ACK recieved:", len(buff))
             print("command time", msec)
             if(buff[1] == command_map['STATUS_ACK']):
                 print_status()
-            print("Cmd_ACK: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
+            #print("Cmd_ACK: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
+            time.sleep(0.005)
             #print(buff)
             return
+        #if(comm_state != sync_state): print("ms: ", msec)
+        elif comm_state != sync_state and msec > message_timeout:
+            missed_packets += 1
+            print("command timeout", msec, len(buff), data_recv, data_total)
+            print("cmd state: ", comm_state)
+            #print("buffer recieved:", buff) 
+            print("Cmd fail: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
+            comm_state = sync_state
+            time.sleep(0.015)
+            return
+
+        elif msec > message_timeout:
+            print("command timeout", msec)
+            print("no cmd state: ", comm_state)
+            print("Cmd fail: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
+            missed_packets += 1
+            time.sleep(0.015)
+            return
+
 
 
 log_begin = time.perf_counter()
@@ -394,8 +413,21 @@ while True:
             read_cmd()
         elif event == '_EXEC_':
             cmd_launched = 1
-            prog = window['_PROG_'].get()
-            cmd = bytearray([0x55, command_map['EXEC_PROG'], 1, prog, 0x20, 0x21])
+            s = window['_PROG_'].get()
+            arr = s.split(',')
+            prog = int(arr[0])
+            p1 = int(arr[1])
+            p2 = int(arr[2])
+            p3 = int(arr[3])
+            l1 = int(arr[4])
+            l2 = int(arr[5])
+            cmd = bytearray([0x55, command_map['EXEC_PROG'], 11, prog, 
+                             (p1 >> 8) & 0xff, (p1) & 0xff, 
+                             (p2 >> 8) & 0xff, (p2) & 0xff, 
+                             (p3 >> 8) & 0xff, (p3) & 0xff, 
+                             (l1 >> 8) & 0xff, (l1) & 0xff, 
+                             (l2 >> 8) & 0xff, (l2) & 0xff, 
+                             0x20, 0x21])
             print("Comand sent", cmd)
             ser.write(cmd)
             read_cmd()
