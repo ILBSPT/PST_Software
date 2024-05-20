a93f8cd722b66d24df1b28e3f359680695be8f95
ILBSPT <andremanada@gmail.com>
2024-05-20 16:52:38 +0100

    max6675 library

--- HelperCode/ESP_LoRa_reciever/src/main.cpp
+++ HelperCode/ESP_LoRa_reciever/src/main.cpp
@@ -1,56 +1,29 @@
 #include <arduino.h>
 
-float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+#include <max6675.h>
+
+
+MAX6675 thermocouple(18, 0, 19);
+
 void setup() {
   Serial.begin(115200);
+  Serial2.begin(9600);
+
   while (!Serial);
+  while (!Serial2);
 
-  //Serial.println("LoRa Receiver");
-
-  /*
-  LoRa.setPins(5,4,36);
-  //LoRa.setSignalBandwidth(500E3);
-  //LoRa.setCodingRate4(5);
-  //LoRa.setSpreadingFactor(7);
-  //LoRa.setGain(1);
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-
-  uint8_t reg = LoRa.readRegister(0x1);
-  Serial.println("Register");
-  Serial.println(reg);
-
-  */
 }
 
 void loop() {
-  //int analogValue = analogRead(39);
-  //float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+    // basic readout test, just print the current temp
+  
+   //Serial.print("C = "); 
+   //Serial.println(thermocouple.readCelsius());
+   //Serial.print("F = ");
+   //Serial.println(thermocouple.readFahrenheit());
+ 
+   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
+   //delay(1000);
 
-  //Serial.print("Value ");
-  //Serial.println(voltage);
-  //delay(100);
-  /*
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
-
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-  }
-  */
-
-
+  while(Serial2.available()) Serial.write(Serial2.read());
 }
