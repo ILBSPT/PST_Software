36225a0393a90527feb790b40228b25ab1dfc89c
ILBSPT <andremanada@gmail.com>
2024-05-22 09:48:39 +0100

    change rs485 baud rate

--- HelperCode/arduino_rs485_sniffer/src/main.cpp
+++ HelperCode/arduino_rs485_sniffer/src/main.cpp
@@ -93,87 +93,90 @@ void write_command(command_t* cmd)
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(115200);
+  //Serial.begin(57600);
+  Serial2.begin(57600);
+  //Serial2.begin(38400);
 
   state = SYNC;
 }
 
 void loop() {
     //while(Serial.available()) Serial2.write(Serial.read());
-    while(Serial2.available() && state != END) 
-    {
-        uint8_t read_byte = Serial2.read();
+    //while(Serial2.available() && state != END) 
+    //{
+        //uint8_t read_byte = Serial2.read();
     
-        switch(state)
-        {
-            case SYNC:
-                if(read_byte == 0x55)
-                {
-                    //start timeout timer
-                    state = CMD;
-                    command.data_recv = 0;
-                    memset(&command, 0, sizeof(command_t));
-                    command.begin = clock();
-                }
-            break;
+        //switch(state)
+        //{
+            //case SYNC:
+                //if(read_byte == 0x55)
+                //{
+                    ////start timeout timer
+                    //state = CMD;
+                    //command.data_recv = 0;
+                    //memset(&command, 0, sizeof(command_t));
+                    //command.begin = clock();
+                //}
+            //break;
 
-            case CMD:
-                command.cmd = (cmd_type_t)read_byte;
-                state = SIZE;
-            break;
+            //case CMD:
+                //command.cmd = (cmd_type_t)read_byte;
+                //state = SIZE;
+            //break;
 
-            case SIZE:                
-                command.size = read_byte;
-                if(command.size == 0)
-                    state = CRC1;
-                else state = DATA;
-            break;
+            //case SIZE:                
+                //command.size = read_byte;
+                //if(command.size == 0)
+                    //state = CRC1;
+                //else state = DATA;
+            //break;
 
-            case DATA:
-                command.data[command.data_recv++] = read_byte;
-                if(command.data_recv == command.size)
-                    state = CRC1;
-            break;
+            //case DATA:
+                //command.data[command.data_recv++] = read_byte;
+                //if(command.data_recv == command.size)
+                    //state = CRC1;
+            //break;
 
-            case CRC1:
-                command.crc = read_byte << 8;
-                state = CRC2;
-            break;
+            //case CRC1:
+                //command.crc = read_byte << 8;
+                //state = CRC2;
+            //break;
 
-            case CRC2:
-                command.crc += read_byte;
-                state = END;
-            break;
+            //case CRC2:
+                //command.crc += read_byte;
+                //state = END;
+            //break;
 
-            default:
-                state = SYNC;
-        };
+            //default:
+                //state = SYNC;
+        //};
 
-    }
+    //}
 
-    end = clock();
-    int msec = (end - command.begin) * 1000 / CLOCKS_PER_SEC;
+    //end = clock();
+    //int msec = (end - command.begin) * 1000 / CLOCKS_PER_SEC;
 
-    //if timeout reset state
-    if(state != SYNC && msec > RS485_TIMEOUT_TIME_MS) //timeout
-    {
-        Serial.println("reset"); //debug
-        state = SYNC;
+    ////if timeout reset state
+    //if(state != SYNC && msec > RS485_TIMEOUT_TIME_MS) //timeout
+    //{
+        //Serial.println("reset"); //debug
+        //state = SYNC;
 
 
-    }
-    //if bad cr reset state
-    else if(state == END /* && check_crc(&command) */)
-    {
-        state = SYNC;
+    //}
+    ////if bad cr reset state
+    //else if(state == END /* && check_crc(&command) */)
+    //{
+        //state = SYNC;
 
-    }
-    else if(state == END)
-    {
-        state = SYNC;
-    }
-    else //default
-    {
-    }
+    //}
+    //else if(state == END)
+    //{
+        //state = SYNC;
+    //}
+    //else //default
+    //{
+    //}
 
+  while(Serial2.available()) Serial.write(Serial2.read());
 }                                                                                                                       
